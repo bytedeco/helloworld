b14deec65eebdbcd1695520a508c1741a0073b6d
Matteo Di Giovinazzo <482618+matteodg@users.noreply.github.com>
2019-05-13 13:52:56 -0600

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -3,3 +3,15 @@
 char * getHelloWorldMessage(void) {
     return HELLO_WORLD;
 }
+
+void printHelloWorldMessage(char * message) {
+    printf("%s\n", message);
+}
+
+int getIntValue() {
+    return 1234;
+}
+
+void printIntValue(int value) {
+    printf("%d\n", value);
+}
