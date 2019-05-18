1c66abb0be5c0fd2250a70bd5cc7403baf9dcca0
Matteo Di Giovinazzo <matteodg@gmail.com>
2019-05-18 15:45:37 -0600

    Added functions for boolean, byte, char, short, int, long, String

--- helloworld.h
+++ helloworld.h
@@ -3,13 +3,26 @@
 
 extern "C"
 {
-    char * getHelloWorldMessage(void);
+    bool getBool();
+    void printBool(bool value);
 
-    void printHelloWorldMessage(char * message);
+    char getByte();
+    void printByte(char value);
 
-    int getIntValue();
+    wchar_t getChar();
+    void printChar(wchar_t value);
 
-    void printIntValue(int value);
+    short int getShort();
+    void printShort(short int value);
+
+    int getInt();
+    void printInt(int value);
+
+    long long int getLong();
+    void printLong(long long int value);
+
+    char * getString(void);
+    void printString(char * message);
 }
 
 #endif // helloworld_h__
