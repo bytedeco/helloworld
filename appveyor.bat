c7b432012a783633f42770daade12d1988744a03
Matteo Di Giovinazzo <482618+matteodg@users.noreply.github.com>
2019-05-30 22:06:47 -0600

    Update appveyor.bat

---
+++ appveyor.bat
@@ -0,0 +1,12 @@
+@echo off
+:: Batch file for building/testing on AppVeyor
+
+cd %APPVEYOR_BUILD_FOLDER%
+PATH C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%
+
+bash -lc "aclocal"
+bash -lc "autoconf"
+bash -lc "automake --add-missing"
+bash -lc "./configure --prefix=`pwd`/build"
+bash -lc "make"
+bash -lc "make distcheck"
