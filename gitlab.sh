8299fdff5157bb0e800fdf98b19fc361b4dac6bc
Matteo Di Giovinazzo <matteodg@gmail.com>
2019-06-18 04:05:22 +0000

    Added script to build on Windows

---
+++ gitlab.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+export PATH="/c/msys64/$MSYSTEM/bin:$PATH"
+if [[ "$MSYSTEM" == "MINGW32" ]]; then
+    export MSYS2_ARCH="i686"
+else
+    export MSYS2_ARCH="x86_64"
+fi
+
+pacman --noconfirm -Suy
+
+pacman --noconfirm -S --needed \
+    base-devel \
+    mingw-w64-$MSYS2_ARCH-toolchain
